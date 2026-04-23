af9bc882aea3e8aa87af6ade72552fb1fb497edc
chuong-dev17 <chuongdang.171003@gmail.com>
2026-04-23 10:16:52 +0700

    fix gitinore

--- Car_Dealership/src/java/daos/PartsDAO.java
+++ Car_Dealership/src/java/daos/PartsDAO.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import dtos.Parts;
+import java.sql.Statement;
 import utils.DBUtil;
 
 /**
@@ -136,41 +137,42 @@ public class PartsDAO {
         }
     }
 
-        public List<Parts> searchPartbyname(String partName) {
-        List<Parts> list = new ArrayList<>();
-        Connection conn = null;
-        String sql = "SELECT [partID]\n"
-                + "      ,[partName]\n"
-                + "      ,[purchasePrice]\n"
-                + "      ,[retailPrice]\n"
-                + "  FROM [Car_Dealership].[dbo].[Parts]"
-                + "where [Status] = 'Active'and partName LIKE ? ;";
-        try {
-            conn = DBUtil.getConnection();
-            if (conn != null) {
-                PreparedStatement st = conn.prepareStatement(sql);
-                st.setString(1, "%" + partName + "%");
-                ResultSet rs = st.executeQuery();
-                while (rs.next()) {
-                    list.add(new Parts(rs.getString("partID"),
-                            rs.getString("partName"),
-                            rs.getString("purchasePrice"),
-                            rs.getString("retailPrice")));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+    public List<Parts> searchPartbyname(String partName) {
+    List<Parts> list = new ArrayList<>();
+    Connection conn = null;
+
+    String sql = "SELECT [partID], [partName], [purchasePrice], [retailPrice] "
+            + "FROM [Car_Dealership].[dbo].[Parts] "
+            + "WHERE [Status] = 'Active' AND partName LIKE '%" + partName + "%'";
+
+    try {
+        conn = DBUtil.getConnection();
+        if (conn != null) {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                list.add(new Parts(
+                        rs.getString("partID"),
+                        rs.getString("partName"),
+                        rs.getString("purchasePrice"),
+                        rs.getString("retailPrice")
+                ));
             }
         }
-        return list;
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+    return list;
+}
 
     public void addParts(Parts p) {
         Connection conn = null;
