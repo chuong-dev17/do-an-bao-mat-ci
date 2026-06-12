87e4b330fbc49e65325f8494648bea068bdfaae2
chuong-dev17 <chuongdang.171003@gmail.com>
2026-06-13 06:50:53 +0700

    test bug

--- Car_Dealership/src/java/utils/DBUtil.java
+++ Car_Dealership/src/java/utils/DBUtil.java
@@ -21,11 +21,11 @@ public class DBUtil {
     
     public static Connection getConnection() throws ClassNotFoundException, SQLException, NamingException {
         Connection conn = null;
-        String username = System.getenv("DB_USERNAME");
-        String password = System.getenv("DB_PASSWORD");
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = System.getenv("DB_URL");
-        conn = DriverManager.getConnection(url, username, password);
+        String username = "sa";
+String password = "12345";
+Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+String url = "jdbc:sqlserver://host.docker.internal:1433;databaseName=Car_Dealership;encrypt=true;trustServerCertificate=true";
+conn = DriverManager.getConnection(url, username, password);
         return conn;
     }
 
